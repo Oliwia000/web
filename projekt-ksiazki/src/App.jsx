a83ff93368024ffad6962d6fb94d916687249047
teren <teren@KOMP>
2025-03-19 19:28:15 +0100

    zmiany

--- projekt-ksiazki/src/App.jsx
+++ projekt-ksiazki/src/App.jsx
@@ -4,10 +4,27 @@ import Nawigacja from "./components/Nawigacja";
 import KategoriaBoczna from "./components/KategoriaBoczna";
 import ListaKsiazek from "./components/ListaKsiazek";
 import SzczegolyKsiazki from "./components/SzczegolyKsiazki";
+import Wyszukiwarka from "./components/Wyszukiwarka"; 
 import { wszystkieKsiazki } from "./data/daneKsiazek"; 
 
 function App() {
   const [wybranaKategoria, setWybranaKategoria] = useState(null);
+  const [query, setQuery] = useState(""); // Dodaj stan do przechowywania zapytania
+
+
+  const handleWyszukaj = (searchQuery) => {
+    setQuery(searchQuery);
+  };
+
+  
+  const ksiazkiDoWyswietlenia = wszystkieKsiazki.filter((ksiazka) => {
+    return (
+      (ksiazka.tytul.toLowerCase().includes(query.toLowerCase()) ||
+        ksiazka.autor.toLowerCase().includes(query.toLowerCase()) ||
+        ksiazka.wydawnictwo.toLowerCase().includes(query.toLowerCase())) &&
+      (wybranaKategoria ? ksiazka.kategorie.includes(wybranaKategoria) : true)
+    );
+  });
 
   return (
     <Router>
@@ -15,10 +32,12 @@ function App() {
         <Nawigacja />
         <div className="main-content">
           <KategoriaBoczna wybierzKategorie={setWybranaKategoria} />
+          {}
+          <Wyszukiwarka onWyszukaj={handleWyszukaj} />
           <Routes>
             <Route
               path="/"
-              element={<ListaKsiazek ksiazki={wszystkieKsiazki} wybranaKategoria={wybranaKategoria} />}
+              element={<ListaKsiazek ksiazki={ksiazkiDoWyswietlenia} wybranaKategoria={wybranaKategoria} />}
             />
             <Route path="/ksiazka/:id" element={<SzczegolyKsiazki />} />
           </Routes>
--- projekt-ksiazki/src/components/ListaKsiazek.jsx
+++ projekt-ksiazki/src/components/ListaKsiazek.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Wyszukiwarka from "./Wyszukiwarka";
 import "../styles/style.css";
 
 const ListaKsiazek = ({ ksiazki, wybranaKategoria }) => {
-  const ksiazkiDoWyswietlenia = wybranaKategoria
-    ? ksiazki.filter((ksiazka) => ksiazka.kategorie.includes(wybranaKategoria))
-    : ksiazki;
-
   return (
     <div className="lista-ksiazek">
-      {ksiazkiDoWyswietlenia.map((ksiazka) => (
+      {ksiazki.map((ksiazka) => (
         <div key={ksiazka.id} className="ksiazka">
           <img src={ksiazka.okladka} alt={ksiazka.tytul} />
           <h4>{ksiazka.tytul}</h4>
